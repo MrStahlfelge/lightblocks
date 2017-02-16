5fd074be0839cecf95df17147658167c2804bae6
Benjamin Schulte <bs@golfgl.de>
2017-02-16 21:46:18 +0100

    Touchpad zur Hilfe

--- core/src/de/golfgl/lightblocks/screen/PlayGesturesInput.java
+++ core/src/de/golfgl/lightblocks/screen/PlayGesturesInput.java
@@ -1,13 +1,14 @@
 package de.golfgl.lightblocks.screen;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.scenes.scene2d.Actor;
 import com.badlogic.gdx.scenes.scene2d.Group;
 import com.badlogic.gdx.scenes.scene2d.ui.Label;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
 import com.badlogic.gdx.utils.Align;
-
-import de.golfgl.lightblocks.LightBlocksGame;
 
 /**
  * Created by Benjamin Schulte on 25.01.2017.
@@ -21,6 +22,20 @@ public class PlayGesturesInput extends PlayScreenInput {
     boolean beganHorizontalMove;
     boolean beganSoftDrop;
     boolean didSomething;
+
+    Group touchPanel;
+    Vector2 touchCoordinates;
+    private Label toTheRight;
+    private Label toTheLeft;
+    private Label toDrop;
+    private Label rotationLabel;
+
+    @Override
+    public void setPlayScreen(PlayScreen playScreen) {
+        super.setPlayScreen(playScreen);
+
+        initializeTouchPanel(playScreen);
+    }
 
     @Override
     public boolean touchDown(int screenX, int screenY, int pointer, int button) {
@@ -38,10 +53,69 @@ public class PlayGesturesInput extends PlayScreenInput {
             didSomething = true;
         } else {
             playScreen.gameModel.setInputFreezeInterval(.1f);
+            setTouchPanel(screenX, screenY);
             didSomething = false;
         }
 
         return true;
+    }
+
+    public void initializeTouchPanel(PlayScreen playScreen) {
+        touchPanel = new Group();
+        touchPanel.setTransform(false);
+        Vector2 touchCoordinates1 = new Vector2(0, 0);
+        touchCoordinates = new Vector2(DRAG_TRESHOLD, 0);
+
+        playScreen.stage.getViewport().unproject(touchCoordinates);
+        playScreen.stage.getViewport().unproject(touchCoordinates1);
+
+        float screenDragTreshold = Math.abs(touchCoordinates.x - touchCoordinates1.x);
+
+        touchPanel.setVisible(false);
+        rotationLabel = new Label(FontAwesome.ROTATE_RIGHT, playScreen.app.skin, FontAwesome.SKIN_FONT_FA);
+
+        float scale = Math.min(1, screenDragTreshold * 1.9f / rotationLabel.getPrefWidth());
+        rotationLabel.setFontScale(scale);
+
+        rotationLabel.setPosition(-rotationLabel.getPrefWidth() / 2, -rotationLabel.getPrefHeight() / 2);
+
+        toTheRight = new Label(FontAwesome.CIRCLE_RIGHT, playScreen.app.skin, FontAwesome.SKIN_FONT_FA);
+        toTheRight.setFontScale(scale);
+        toTheRight.setPosition(screenDragTreshold, -toTheRight.getPrefHeight() / 2);
+        toTheLeft = new Label(FontAwesome.CIRCLE_LEFT, playScreen.app.skin, FontAwesome.SKIN_FONT_FA);
+        toTheLeft.setFontScale(scale);
+        toTheLeft.setPosition(-screenDragTreshold - toTheRight.getPrefWidth(), -toTheRight.getPrefHeight() / 2);
+
+        toDrop = new Label(FontAwesome.CIRCLE_DOWN, playScreen.app.skin, FontAwesome.SKIN_FONT_FA);
+        toDrop.setFontScale(scale);
+        toDrop.setPosition(-toDrop.getPrefWidth() / 2, -screenDragTreshold - toDrop.getPrefHeight());
+
+        touchPanel.addActor(toTheLeft);
+        touchPanel.addActor(toTheRight);
+        touchPanel.addActor(toDrop);
+        touchPanel.addActor(rotationLabel);
+
+        playScreen.stage.addActor(touchPanel);
+    }
+
+    public void setTouchPanel(int screenX, int screenY) {
+        touchCoordinates.set(screenX, screenY);
+        playScreen.stage.getViewport().unproject(touchCoordinates);
+        touchPanel.setPosition(touchCoordinates.x, touchCoordinates.y);
+
+        Color c = toTheRight.getColor();
+        if (this.screenX >= Gdx.graphics.getWidth() / 2) {
+            c.set(.1f, 1, .3f, .8f);
+            rotationLabel.setText(FontAwesome.ROTATE_RIGHT);
+        } else {
+            c.set(.2f, .8f, 1, .8f);
+            rotationLabel.setText(FontAwesome.ROTATE_LEFT);
+        }
+
+        toTheLeft.setColor(c);
+        toDrop.setColor(c);
+        rotationLabel.setColor(c);
+        touchPanel.setVisible(true);
     }
 
     @Override
@@ -89,9 +163,10 @@ public class PlayGesturesInput extends PlayScreenInput {
     public boolean touchUp(int screenX, int screenY, int pointer, int button) {
 
         playScreen.gameModel.setInputFreezeInterval(0);
+        touchPanel.setVisible(false);
 
         if (!didSomething)
-            playScreen.gameModel.setRotate(screenX > LightBlocksGame.nativeGameWidth / 2);
+            playScreen.gameModel.setRotate(screenX >= Gdx.graphics.getWidth() / 2);
 
         else
             playScreen.gameModel.setSoftDropFactor(0);
