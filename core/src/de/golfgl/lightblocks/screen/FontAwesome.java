eb18d7d7b48365e9960db7ebca4bb758a90b4389
Benjamin Schulte <bs@golfgl.de>
2017-03-26 12:50:00 +0200

    FA: GPGS Logo, Achievement und Leaderboard Symbol

--- core/src/de/golfgl/lightblocks/screen/FontAwesome.java
+++ core/src/de/golfgl/lightblocks/screen/FontAwesome.java
@@ -8,109 +8,113 @@ public class FontAwesome {
 
     public static final String SKIN_FONT_FA = "fontawesome";
 
-    public static String ROTATE_RIGHT = "\uF01E";
-    public static String ROTATE_LEFT = "\uF0E2";
-    public static String ROTATE_RELOAD = "\uF021";
+    public static final String GPGS_LOGO = "A";
+    public static final String GPGS_LEADERBOARD = "B";
+    public static final String GPGS_ACHIEVEMENT = "C";
 
-    public static String BIG_PLAY = "\uF04B";
-    public static String BIG_PAUSE = "\uF04C";
-    public static String BIG_STOP = "\uF04D";
-    public static String BIG_BACKWARD = "\uF04A";
-    public static String BIG_FORWARD = "\uF04E";
-    public static String BIG_FASTBW = "\uF049";
-    public static String BIG_FASTFW = "\uF050";
+    public static final String ROTATE_RIGHT = "\uF01E";
+    public static final String ROTATE_LEFT = "\uF0E2";
+    public static final String ROTATE_RELOAD = "\uF021";
 
-    public static String RIGHT_CHEVRON = "\uF054";
-    public static String LEFT_CHEVRON = "\uF053";
-    public static String UP_CHEVRON = "\uF077";
-    public static String DOWN_CHEVRON = "\uF078";
+    public static final String BIG_PLAY = "\uF04B";
+    public static final String BIG_PAUSE = "\uF04C";
+    public static final String BIG_STOP = "\uF04D";
+    public static final String BIG_BACKWARD = "\uF04A";
+    public static final String BIG_FORWARD = "\uF04E";
+    public static final String BIG_FASTBW = "\uF049";
+    public static final String BIG_FASTFW = "\uF050";
 
-    public static String RIGHT_CARET = "\uF0DA";
-    public static String LEFT_CARET = "\uF0D9";
-    public static String UP_CARET = "\uF0D8";
-    public static String DOWN_CARET = "\uF0D7";
+    public static final String RIGHT_CHEVRON = "\uF054";
+    public static final String LEFT_CHEVRON = "\uF053";
+    public static final String UP_CHEVRON = "\uF077";
+    public static final String DOWN_CHEVRON = "\uF078";
 
-    public static String RIGHT_ARROW = "\uF061";
-    public static String LEFT_ARROW = "\uF060";
-    public static String UP_ARROW = "\uF062";
-    public static String DOWN_ARROW = "\uF063";
+    public static final String RIGHT_CARET = "\uF0DA";
+    public static final String LEFT_CARET = "\uF0D9";
+    public static final String UP_CARET = "\uF0D8";
+    public static final String DOWN_CARET = "\uF0D7";
 
-    public static String CIRCLE_LEFT = "\uF137";
-    public static String CIRCLE_RIGHT = "\uF138";
-    public static String CIRCLE_UP = "\uF139";
-    public static String CIRCLE_DOWN = "\uF13A";
-    public static String CIRCLE_PLAY = "\uF144";
-    public static String CIRCLE_PAUSE = "\uF28B";
-    public static String CIRCLE_STOP = "\uF28D";
-    public static String CIRCLE_CROSS = "\uF057";
-    public static String CIRCLE_CHECK = "\uF058";
-    public static String CIRCLE_EMPTY = "\uF111";
+    public static final String RIGHT_ARROW = "\uF061";
+    public static final String LEFT_ARROW = "\uF060";
+    public static final String UP_ARROW = "\uF062";
+    public static final String DOWN_ARROW = "\uF063";
 
-    public static String MENU_TREE = "\uF0E8";
-    public static String MENU_SANDWICH = "\uF0C9";
-    public static String MENU_BULLETS = "\uF0CA";
-    public static String MENU_SIDEBAR = "\uF142";
-    public static String MENU_BIGBULLETS = "\uF00B";
+    public static final String CIRCLE_LEFT = "\uF137";
+    public static final String CIRCLE_RIGHT = "\uF138";
+    public static final String CIRCLE_UP = "\uF139";
+    public static final String CIRCLE_DOWN = "\uF13A";
+    public static final String CIRCLE_PLAY = "\uF144";
+    public static final String CIRCLE_PAUSE = "\uF28B";
+    public static final String CIRCLE_STOP = "\uF28D";
+    public static final String CIRCLE_CROSS = "\uF057";
+    public static final String CIRCLE_CHECK = "\uF058";
+    public static final String CIRCLE_EMPTY = "\uF111";
 
-    public static String SQUARE_CHECKED = "\uF046";
-    public static String SQUARE_EMPTY = "\uF096";
-    public static String SQUARE_PENCIL = "\uF044";
+    public static final String MENU_TREE = "\uF0E8";
+    public static final String MENU_SANDWICH = "\uF0C9";
+    public static final String MENU_BULLETS = "\uF0CA";
+    public static final String MENU_SIDEBAR = "\uF142";
+    public static final String MENU_BIGBULLETS = "\uF00B";
 
-    public static String SETTING_PENCIL = "\uF040";
-    public static String SETTINGS_SPEAKER_ON = "\uF028";
-    public static String SETTINGS_SPEAKER_OFF = "\uF026";
-    public static String SETTINGS_MUSIC = "\uF001";
-    public static String SETTINGS_GEAR = "\uF013";
-    public static String SETTINGS_GEARS = "\uF085";
-    public static String SETTINGS_SLIDERS = "\uF1DE";
-    public static String SETTINGS_SLIDER_ON = "\uF205";
-    public static String SETTINGS_SLIDER_OFF = "\uF204";
-    public static String SETTINGS_WRENCH = "\uF0AD";
+    public static final String SQUARE_CHECKED = "\uF046";
+    public static final String SQUARE_EMPTY = "\uF096";
+    public static final String SQUARE_PENCIL = "\uF044";
 
-    public static String DEVICE_POWERSWITCH = "\uF011";
-    public static String DEVICE_LAPTOP = "\uF109";
-    public static String DEVICE_GAMEPAD = "\uF11B";
-    public static String DEVICE_KEYBOARD = "\uF11C";
-    public static String DEVICE_GESTURE1 = "\uF0A6";
-    public static String DEVICE_GESTURE2 = "\uF25A";
-    public static String DEVICE_TABLET = "\uF10A";
-    public static String DEVICE_MOBILEPHONE = "\uF10B";
-    public static String DEVICE_ANDROID = "\uF17B";
-    public static String DEVICE_GRAVITY = "\uF24E";
+    public static final String SETTING_PENCIL = "\uF040";
+    public static final String SETTINGS_SPEAKER_ON = "\uF028";
+    public static final String SETTINGS_SPEAKER_OFF = "\uF026";
+    public static final String SETTINGS_MUSIC = "\uF001";
+    public static final String SETTINGS_GEAR = "\uF013";
+    public static final String SETTINGS_GEARS = "\uF085";
+    public static final String SETTINGS_SLIDERS = "\uF1DE";
+    public static final String SETTINGS_SLIDER_ON = "\uF205";
+    public static final String SETTINGS_SLIDER_OFF = "\uF204";
+    public static final String SETTINGS_WRENCH = "\uF0AD";
 
-    public static String NET_LOGIN = "\uF090";
-    public static String NET_LOGOUT = "\uF08B";
-    public static String NET_GOOGLEPLUS = "\uF0D5";
-    public static String NET_FACEBOOK = "\uF09A";
-    public static String NET_TWITTER = "\uF099";
-    public static String NET_CLOUDSAVE = "\uF0EE";
-    public static String NET_SQUARELINK = "\uF08E";
-    public static String NET_PEOPLE = "\uF0C0";
-    public static String NET_PERSON = "\uF007";
-    public static String NET_ADDPERSON = "\uF234";
-    public static String NET_UNLOCKED = "\uF13E";
-    public static String NET_LOCKED = "\uF023";
-    public static String NET_SHARE1 = "\uF1E0";
-    public static String NET_SHARE2 = "\uF064";
+    public static final String DEVICE_POWERSWITCH = "\uF011";
+    public static final String DEVICE_LAPTOP = "\uF109";
+    public static final String DEVICE_GAMEPAD = "\uF11B";
+    public static final String DEVICE_KEYBOARD = "\uF11C";
+    public static final String DEVICE_GESTURE1 = "\uF0A6";
+    public static final String DEVICE_GESTURE2 = "\uF25A";
+    public static final String DEVICE_TABLET = "\uF10A";
+    public static final String DEVICE_MOBILEPHONE = "\uF10B";
+    public static final String DEVICE_ANDROID = "\uF17B";
+    public static final String DEVICE_GRAVITY = "\uF24E";
 
-    public static String MISC_ATTACH = "\uF0C6";
-    public static String MISC_PLUS = "\uF067";
-    public static String MISC_MINUS = "\uF068";
-    public static String MISC_CROSS = "\uF00D";
-    public static String MISC_PIECE = "\uF12E";
+    public static final String NET_LOGIN = "\uF090";
+    public static final String NET_LOGOUT = "\uF08B";
+    public static final String NET_GOOGLEPLUS = "\uF0D5";
+    public static final String NET_FACEBOOK = "\uF09A";
+    public static final String NET_TWITTER = "\uF099";
+    public static final String NET_CLOUDSAVE = "\uF0EE";
+    public static final String NET_SQUARELINK = "\uF08E";
+    public static final String NET_PEOPLE = "\uF0C0";
+    public static final String NET_PERSON = "\uF007";
+    public static final String NET_ADDPERSON = "\uF234";
+    public static final String NET_UNLOCKED = "\uF13E";
+    public static final String NET_LOCKED = "\uF023";
+    public static final String NET_SHARE1 = "\uF1E0";
+    public static final String NET_SHARE2 = "\uF064";
 
-    public static String COMMENT_STAR_FULL = "\uF005";
-    public static String COMMENT_STAR_HALF = "\uF123";
-    public static String COMMENT_STAR_EMPTY = "\uF006";
-    public static String COMMENT_STAR_HEART = "\uF004";
-    public static String COMMENT_STAR_TROPHY = "\uF091";
-    public static String COMMENT_STAR_FLAG = "\uF11D";
+    public static final String MISC_ATTACH = "\uF0C6";
+    public static final String MISC_PLUS = "\uF067";
+    public static final String MISC_MINUS = "\uF068";
+    public static final String MISC_CROSS = "\uF00D";
+    public static final String MISC_PIECE = "\uF12E";
 
-    public static String COMMENT_FACE_BAD = "\uF119";
-    public static String COMMENT_FACE_GOOD = "\uF118";
-    public static String COMMENT_FACE_UNDECIDED = "\uF11A";
+    public static final String COMMENT_STAR_FULL = "\uF005";
+    public static final String COMMENT_STAR_HALF = "\uF123";
+    public static final String COMMENT_STAR_EMPTY = "\uF006";
+    public static final String COMMENT_STAR_HEART = "\uF004";
+    public static final String COMMENT_STAR_TROPHY = "\uF091";
+    public static final String COMMENT_STAR_FLAG = "\uF11D";
 
-    public static String COMMENT_THUMB_DOWN = "\uF165";
-    public static String COMMENT_THUMB_UP = "\uF164";
+    public static final String COMMENT_FACE_BAD = "\uF119";
+    public static final String COMMENT_FACE_GOOD = "\uF118";
+    public static final String COMMENT_FACE_UNDECIDED = "\uF11A";
+
+    public static final String COMMENT_THUMB_DOWN = "\uF165";
+    public static final String COMMENT_THUMB_UP = "\uF164";
 
 }
