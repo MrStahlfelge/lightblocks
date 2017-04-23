8d780bc1f8b996e81ae420c779d49d2c417e8629
Benjamin Schulte <bs@golfgl.de>
2017-04-23 15:51:20 +0200

    GarbageModel, Mission 9

---
+++ core/src/de/golfgl/lightblocks/model/GarbageModel.java
@@ -0,0 +1,56 @@
+package de.golfgl.lightblocks.model;
+
+import com.badlogic.gdx.math.MathUtils;
+import com.badlogic.gdx.utils.Json;
+import com.badlogic.gdx.utils.JsonValue;
+
+/**
+ * Mission mit Garbage
+ * <p>
+ * Created by Benjamin Schulte on 23.04.2017.
+ */
+
+public class GarbageModel extends MissionModel {
+    private float garbageChance;
+
+    @Override
+    public String getGoalDescription() {
+        return "goalGarbage";
+    }
+
+    @Override
+    public String[] getGoalParams() {
+        return new String[]{String.valueOf(getMaxBlocksToUse())};
+    }
+
+    @Override
+    protected void activeTetrominoDropped() {
+        if (getMaxBlocksToUse() <= getScore().getDrawnTetrominos())
+            setGameOverWon(IGameModelListener.MotivationTypes.gameSuccess);
+    }
+
+    @Override
+    protected int[] drawGarbageLines() {
+        int[] retVal = null;
+
+        if (MathUtils.randomBoolean(garbageChance)) {
+            retVal = new int[1];
+            retVal[0] = 0;
+        }
+
+        return retVal;
+    }
+
+    @Override
+    public void read(Json json, JsonValue jsonData) {
+        super.read(json, jsonData);
+        this.garbageChance = jsonData.getFloat("garbageChance", 0.5f);
+    }
+
+    @Override
+    public void write(Json json) {
+        super.write(json);
+        json.writeValue("garbageChance", garbageChance);
+    }
+
+}
