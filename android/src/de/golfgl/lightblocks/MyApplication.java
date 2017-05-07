9ba576c637783d2612bab24ea830e1e8de29f455
Benjamin Schulte <bs@golfgl.de>
2017-05-07 21:03:50 +0200

    Bei Crash Logcat Report mit aufnehmen

--- android/src/de/golfgl/lightblocks/MyApplication.java
+++ android/src/de/golfgl/lightblocks/MyApplication.java
@@ -3,8 +3,10 @@ package de.golfgl.lightblocks;
 import android.app.Application;
 import android.content.Context;
 
-import org.acra.*;
-import org.acra.annotation.*;
+import org.acra.ACRA;
+import org.acra.ReportField;
+import org.acra.ReportingInteractionMode;
+import org.acra.annotation.ReportsCrashes;
 
 
 /**
@@ -12,8 +14,12 @@ import org.acra.annotation.*;
  */
 
 @ReportsCrashes(mailTo = "lightblocks@golfgl.de",
-        mode = ReportingInteractionMode.TOAST,
-        resToastText = R.string.crash_toast_text)
+        mode = ReportingInteractionMode.DIALOG,
+        customReportContent = {ReportField.APP_VERSION_CODE, ReportField.APP_VERSION_NAME, ReportField
+                .ANDROID_VERSION, ReportField.PHONE_MODEL, ReportField.CUSTOM_DATA, ReportField.STACK_TRACE,
+                ReportField.LOGCAT},
+        resToastText = R.string.crash_toast_text,
+        resDialogText = R.string.crash_dialog_text)
 public class MyApplication extends Application {
     @Override
     protected void attachBaseContext(Context newBase) {
