a9e56cabf6e850f2e9df9941bd9455a929dffac6
Benjamin Schulte <bs@golfgl.de>
2018-02-03 20:07:35 +0100

    DesktopLauncher MockClient

--- desktop/src/de/golfgl/lightblocks/desktop/DesktopLauncher.java
+++ desktop/src/de/golfgl/lightblocks/desktop/DesktopLauncher.java
@@ -2,13 +2,73 @@ package de.golfgl.lightblocks.desktop;
 
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.badlogic.gdx.utils.Array;
+
+import de.golfgl.gdxgamesvcs.IGameServiceClient;
+import de.golfgl.gdxgamesvcs.MockGameServiceClient;
+import de.golfgl.gdxgamesvcs.achievement.IAchievement;
+import de.golfgl.gdxgamesvcs.gamestate.ISaveGameStateResponseListener;
+import de.golfgl.gdxgamesvcs.leaderboard.ILeaderBoardEntry;
 import de.golfgl.lightblocks.LightBlocksGame;
+import de.golfgl.lightblocks.gpgs.IGpgsClient;
+import de.golfgl.lightblocks.multiplayer.AbstractMultiplayerRoom;
 
 public class DesktopLauncher {
-	public static void main (String[] arg) {
-		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		config.width = LightBlocksGame.nativeGameWidth;
-		config.height = LightBlocksGame.nativeGameHeight;
-		new LwjglApplication(new LightBlocksGame(), config);
-	}
+    public static void main(String[] arg) {
+        LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+        config.width = 540; //LightBlocksGame.nativeGameWidth;
+        config.height = 960; //LightBlocksGame.nativeGameHeight;
+        LightBlocksGame game = new LightBlocksGame();
+        game.gpgsClient = new MyTestClient();
+        game.gpgsClient.setListener(game);
+        new LwjglApplication(game, config);
+    }
+
+    private static class MyTestClient extends MockGameServiceClient implements IGpgsClient {
+
+        public MyTestClient() {
+            super(.5f);
+        }
+
+        @Override
+        protected String getPlayerName() {
+            return "12345678901234567890";
+        }
+
+        @Override
+        public AbstractMultiplayerRoom createMultiPlayerRoom() {
+            return null;
+        }
+
+        @Override
+        public Boolean saveGameStateSync(String id, byte[] gameState, long progressValue,
+                                         ISaveGameStateResponseListener listener) {
+            return null;
+        }
+
+        @Override
+        protected Array<ILeaderBoardEntry> getLeaderboardEntries() {
+            return null;
+        }
+
+        @Override
+        protected Array<String> getGameStates() {
+            return null;
+        }
+
+        @Override
+        protected byte[] getGameState() {
+            return new byte[0];
+        }
+
+        @Override
+        protected Array<IAchievement> getAchievements() {
+            return null;
+        }
+
+        @Override
+        public String getGameServiceId() {
+            return IGameServiceClient.GS_AMAZONGC_ID;
+        }
+    }
 }
