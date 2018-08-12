f06927ffdc058243fab2c63423b773bbb60f7dae
Benjamin Schulte <bs@golfgl.de>
2018-08-12 20:11:08 +0200

    v1.2.1831

--- core/src/de/golfgl/lightblocks/LightBlocksGame.java
+++ core/src/de/golfgl/lightblocks/LightBlocksGame.java
@@ -54,8 +54,8 @@ public class LightBlocksGame extends Game implements IGameServiceListener {
     public static final String GAME_URL = "https://www.golfgl.de/lightblocks/";
     public static final String CONTROLLER_RECOMMENDATION_URL = "https://www.golfgl.de/lightblocks/#controller";
     // An den gleichen Eintrag im AndroidManifest denken!!!
-    public static final String GAME_VERSIONSTRING = "1.2.1830";
-    public static final int GAME_VERSIONNUMBER = 1830;
+    public static final String GAME_VERSIONSTRING = "1.2.1831";
+    public static final int GAME_VERSIONNUMBER = 1831;
     // Abstand für Git
     // auch dran denken das data-Verzeichnis beim release wegzunehmen!
     public static final boolean GAME_DEVMODE = true;
