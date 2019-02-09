65092f27f82730b1317874c8611a629099d99300
Benjamin Schulte <bs@golfgl.de>
2019-02-09 10:19:41 +0100

    iOS project added

---
+++ ios/src/de/golfgl/lightblocks/IOSLauncher.java
@@ -0,0 +1,21 @@
+package de.golfgl.lightblocks;
+
+import com.badlogic.gdx.backends.iosrobovm.IOSApplication;
+import com.badlogic.gdx.backends.iosrobovm.IOSApplicationConfiguration;
+
+import org.robovm.apple.foundation.NSAutoreleasePool;
+import org.robovm.apple.uikit.UIApplication;
+
+public class IOSLauncher extends IOSApplication.Delegate {
+    public static void main(String[] argv) {
+        NSAutoreleasePool pool = new NSAutoreleasePool();
+        UIApplication.main(argv, null, IOSLauncher.class);
+        pool.close();
+    }
+
+    @Override
+    protected IOSApplication createApplication() {
+        IOSApplicationConfiguration config = new IOSApplicationConfiguration();
+        return new IOSApplication(new LightBlocksGame(), config);
+    }
+}
