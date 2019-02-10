539a4dbadd2527f04a09fe9289f1af021e9df0a7
Benjamin Schulte <bs@golfgl.de>
2019-02-10 12:21:01 +0100

    iOS GA Deviceinfos, Crashreporting, ShareHandler

--- ios/src/de/golfgl/lightblocks/IOSLauncher.java
+++ ios/src/de/golfgl/lightblocks/IOSLauncher.java
@@ -1,10 +1,16 @@
 package de.golfgl.lightblocks;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
 import com.badlogic.gdx.backends.iosrobovm.IOSApplication;
 import com.badlogic.gdx.backends.iosrobovm.IOSApplicationConfiguration;
 
+import org.robovm.apple.foundation.NSArray;
 import org.robovm.apple.foundation.NSAutoreleasePool;
+import org.robovm.apple.foundation.NSString;
+import org.robovm.apple.uikit.UIActivityViewController;
 import org.robovm.apple.uikit.UIApplication;
+import org.robovm.apple.uikit.UIDevice;
 
 public class IOSLauncher extends IOSApplication.Delegate {
     public static void main(String[] argv) {
@@ -22,10 +28,50 @@ public class IOSLauncher extends IOSApplication.Delegate {
             public String getSoundAssetFilename(String name) {
                 return "sound/" + name + ".mp3";
             }
+
+            @Override
+            public void lockOrientation(Input.Orientation orientation) {
+                //UIDevice.getCurrentDevice().getOrientation()
+                //if (orientation == null)
+                // TODO setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LOCKED);
+                //else if (orientation.equals(Input.Orientation.Landscape))
+                // TODO setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+                //else
+                // TODO setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+            }
+
+            @Override
+            public void unlockOrientation() {
+                // TODO setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_UNSPECIFIED);
+            }
+
+            @Override
+            public float getDisplayDensityRatio() {
+                // Die Bezugsgröße war das Moto G mit 320 dpi
+                // dm.xdpi / 320f
+                // IOSGraphics teilt bereits durch 160, also nur noch durch 2 teilen
+                return Gdx.graphics.getDensity() / 2f;
+            }
         };
 
+        // Initialize Android dependant classes
+        game.share = new IosShareHandler();
         game.gameAnalytics = new IosGameAnalytics();
+        ((IosGameAnalytics) game.gameAnalytics).registerUncaughtExceptionHandler();
+
+        // Gerätemodell wird für den Spielernamen benötigt
+        game.modelNameRunningOn = UIDevice.getCurrentDevice().getModel();
 
         return new IOSApplication(game, config);
+    }
+
+    private static class IosShareHandler extends ShareHandler {
+        @Override
+        public void shareText(String message, String title) {
+            NSString textShare = new NSString(message);
+            NSArray texttoshare = new NSArray(textShare);
+            UIActivityViewController share = new UIActivityViewController(texttoshare, null);
+            ((IOSApplication) Gdx.app).getUIViewController().presentViewController(share, true, null);
+        }
     }
 }
--- ios/src/de/golfgl/lightblocks/IosGameAnalytics.java
+++ ios/src/de/golfgl/lightblocks/IosGameAnalytics.java
@@ -1,6 +1,12 @@
 package de.golfgl.lightblocks;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.backends.iosrobovm.custom.HWMachine;
+
 import org.robovm.apple.uikit.UIDevice;
+
+import java.io.PrintWriter;
+import java.io.StringWriter;
 
 import de.golfgl.gdxgameanalytics.GameAnalytics;
 
@@ -8,15 +14,41 @@ public class IosGameAnalytics extends GameAnalytics {
 
     @Override
     public void startSession() {
-        this.setPlatform(Platform.Android);
+        this.setPlatform(Platform.iOS);
         UIDevice currentDevice = UIDevice.getCurrentDevice();
         if (!this.setPlatformVersionString(currentDevice.getSystemVersion())) {
             this.setPlatformVersionString("0");
         }
 
-        this.setDevice(currentDevice.getModel());
+        this.setDevice(HWMachine.getMachineString());
         this.setManufacturer("Apple");
         super.startSession();
+    }
+
+    /**
+     * Registers a handler for catching all uncaught exceptions to send them to GA. Exits the app afterwards
+     */
+    public void registerUncaughtExceptionHandler() {
+        Thread.setDefaultUncaughtExceptionHandler(new Thread.UncaughtExceptionHandler() {
+            public void uncaughtException(Thread t, Throwable e) {
+                try {
+                    StringWriter sw = new StringWriter();
+                    e.printStackTrace(new PrintWriter(sw));
+                    String exceptionAsString = sw.toString();
+                    submitErrorEvent(ErrorType.error, exceptionAsString);
+                    flushQueueImmediately();
+
+                    for (int waitTime = 0; flushingQueue && waitTime < 10; ++waitTime) {
+                        Thread.sleep(100L);
+                    }
+                } catch (Throwable throwable) {
+                    // do nothing
+                } finally {
+                    Gdx.app.exit();
+                }
+
+            }
+        });
     }
 
 
