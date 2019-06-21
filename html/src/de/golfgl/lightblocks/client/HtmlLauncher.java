f4ed87d4650591b555185fd443e316e1143a91a6
Benjamin Schulte <bs@golfgl.de>
2019-06-21 22:16:07 +0200

    HTML: Preload-Logo im Backend anpassbar

--- html/src/de/golfgl/lightblocks/client/HtmlLauncher.java
+++ html/src/de/golfgl/lightblocks/client/HtmlLauncher.java
@@ -5,12 +5,12 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.backends.gwt.GwtApplication;
 import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
 import com.badlogic.gdx.backends.gwt.preloader.Preloader;
-import com.google.gwt.canvas.client.Canvas;
-import com.google.gwt.canvas.dom.client.Context2d;
-import com.google.gwt.canvas.dom.client.CssColor;
+import com.google.gwt.core.client.GWT;
+import com.google.gwt.dom.client.Style;
 import com.google.gwt.event.logical.shared.ResizeEvent;
 import com.google.gwt.event.logical.shared.ResizeHandler;
 import com.google.gwt.user.client.Window;
+import com.google.gwt.user.client.ui.Panel;
 
 import de.golfgl.gdxgameanalytics.GwtGameAnalytics;
 import de.golfgl.gdxgamesvcs.GameJoltClient;
@@ -80,44 +80,19 @@ public class HtmlLauncher extends GwtApplication {
 
     @Override
     public Preloader.PreloaderCallback getPreloaderCallback() {
-        final Canvas canvas = Canvas.createIfSupported();
-        canvas.setWidth("" + (int) (LightBlocksGame.nativeGameWidth * 0.7f) + "px");
-        canvas.setHeight("70px");
-        getRootPanel().add(canvas);
-        final Context2d context = canvas.getContext2d();
-        context.setTextAlign(Context2d.TextAlign.CENTER);
-        context.setTextBaseline(Context2d.TextBaseline.MIDDLE);
-        context.setFont("18pt Calibri");
+        return createPreloaderPanel(GWT.getHostPageBaseURL() + "preloadlogo.png");
+    }
 
-        return new Preloader.PreloaderCallback() {
-            @Override
-            public void update(Preloader.PreloaderState state) {
-                if (state.hasEnded()) {
-                    context.fillRect(0, 0, 300, 40);
-                } else {
-                    System.out.println("loaded " + state.getProgress());
-                    CssColor color = CssColor.make(30, 30, 30);
-                    context.setFillStyle(color);
-                    context.setStrokeStyle(color);
-                    context.fillRect(0, 0, 300, 70);
-                    color = CssColor.make(200, 200, 200); //, (((TimeUtils.nanoTime() - loadStart) % 1000000000) /
-                    // 1000000000f));
-                    context.setFillStyle(color);
-                    context.setStrokeStyle(color);
-                    context.fillRect(0, 0, 300 * (state.getDownloadedSize() / (float) state.getTotalSize()) * 0.97f,
-                            70);
-
-                    context.setFillStyle(CssColor.make(50, 50, 50));
-                    context.fillText("loading", 300 / 2, 70 / 2);
-
-                }
-            }
-
-            @Override
-            public void error(String file) {
-                System.out.println("error: " + file);
-            }
-        };
+    @Override
+    protected void adjustMeterPanel(Panel meterPanel, Style meterStyle) {
+        meterPanel.setStyleName("gdx-meter");
+        meterPanel.addStyleName("nostripes");
+        Style meterPanelStyle = meterPanel.getElement().getStyle();
+        meterPanelStyle.setProperty("padding", "0px");
+        meterPanelStyle.setProperty("WebkitBoxShadow", "none");
+        meterPanelStyle.setProperty("boxShadow", "none");
+        meterStyle.setProperty("backgroundColor", "#ffffff");
+        meterStyle.setProperty("backgroundImage", "none");
     }
 
     class ResizeListener implements ResizeHandler {
