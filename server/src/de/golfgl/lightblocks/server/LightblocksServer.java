c3f53794d50598005c01111a9ebc31d328b57119
Benjamin Schulte <bs@golfgl.de>
2020-11-02 21:54:43 +0100

    Server: Improved skeleton and introduced handshake to client

--- server/src/de/golfgl/lightblocks/server/LightblocksServer.java
+++ server/src/de/golfgl/lightblocks/server/LightblocksServer.java
@@ -1,18 +1,27 @@
 package de.golfgl.lightblocks.server;
 
+import com.badlogic.gdx.Application;
 import com.badlogic.gdx.ApplicationListener;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.backends.headless.HeadlessApplication;
 import com.badlogic.gdx.backends.headless.HeadlessApplicationConfiguration;
+import com.badlogic.gdx.utils.Json;
+import com.badlogic.gdx.utils.JsonWriter;
 
 import org.java_websocket.WebSocket;
 import org.java_websocket.handshake.ClientHandshake;
 import org.java_websocket.server.WebSocketServer;
 
+import java.io.IOException;
 import java.net.InetSocketAddress;
 import java.nio.ByteBuffer;
 
+import de.golfgl.lightblocks.server.model.Handshake;
+
 public class LightblocksServer extends WebSocketServer implements ApplicationListener {
+
+    private final Handshake handshake = new Handshake();
+    private final Json json = new Json();
 
     public LightblocksServer(InetSocketAddress address) {
         super(address);
@@ -24,68 +33,86 @@ public class LightblocksServer extends WebSocketServer implements ApplicationLis
 
         LightblocksServer server = new LightblocksServer(new InetSocketAddress(port));
         new HeadlessApplication(server, config);
+
+        // this will block when successful, so don't do it inside create()
         server.run();
+
+        Gdx.app.exit();
     }
 
     @Override
     public void create() {
-        Gdx.app.log("Server", "Created.");
-    }
+        Gdx.app.log("Server", "Starting, binding on port " + getPort());
+        Gdx.app.setLogLevel(Application.LOG_DEBUG);
 
-    @Override
-    public void resize(int width, int height) {
+        handshake.authRequired = false;
+        handshake.name = "Lightblocks Server";
+        handshake.owner = "me";
+        handshake.version = 1;
 
-    }
-
-    @Override
-    public void render() {
-
-    }
-
-    @Override
-    public void pause() {
-
-    }
-
-    @Override
-    public void resume() {
-
-    }
-
-    @Override
-    public void dispose() {
-        Gdx.app.log("Server", "Closed.");
-    }
-
-    @Override
-    public void onOpen(WebSocket conn, ClientHandshake handshake) {
-        conn.send("Welcome to the server!"); //This method sends a message to the new client
-        broadcast("new connection: " + handshake.getResourceDescriptor()); //This method sends a message to all clients connected
-        System.out.println("new connection to " + conn.getRemoteSocketAddress());
-    }
-
-    @Override
-    public void onClose(WebSocket conn, int code, String reason, boolean remote) {
-        System.out.println("closed " + conn.getRemoteSocketAddress() + " with exit code " + code + " additional info: " + reason);
-    }
-
-    @Override
-    public void onMessage(WebSocket conn, String message) {
-        System.out.println("received message from " + conn.getRemoteSocketAddress() + ": " + message);
-    }
-
-    @Override
-    public void onMessage(WebSocket conn, ByteBuffer message) {
-        System.out.println("received ByteBuffer from " + conn.getRemoteSocketAddress());
-    }
-
-    @Override
-    public void onError(WebSocket conn, Exception ex) {
-        System.err.println("an error occurred on connection " + conn.getRemoteSocketAddress() + ":" + ex);
+        json.setOutputType(JsonWriter.OutputType.json);
     }
 
     @Override
     public void onStart() {
-        System.out.println("server started successfully");
+        Gdx.app.log("Server", "server started successfully, listening on port " + getPort());
+    }
+
+    @Override
+    public void resize(int width, int height) {
+        // will not happen
+    }
+
+    @Override
+    public void render() {
+        // update game state here
+    }
+
+    @Override
+    public void pause() {
+        // not needed
+    }
+
+    @Override
+    public void resume() {
+        // will not happen
+    }
+
+    @Override
+    public void dispose() {
+        try {
+            stop();
+        } catch (IOException | InterruptedException e) {
+            Gdx.app.error("Server", "Error while stopping server.", e);
+        }
+        Gdx.app.log("Server", "Stopped.");
+    }
+
+    @Override
+    public void onOpen(WebSocket conn, ClientHandshake handshake) {
+        synchronized (json) {
+            conn.send(json.toJson(this.handshake)); //This method sends a message to the new client
+            Gdx.app.debug("Server", "new connection to " + conn.getRemoteSocketAddress());
+        }
+    }
+
+    @Override
+    public void onClose(WebSocket conn, int code, String reason, boolean remote) {
+        Gdx.app.debug("Server", "closed " + conn.getRemoteSocketAddress() + " with exit code " + code + " additional info: " + reason);
+    }
+
+    @Override
+    public void onMessage(WebSocket conn, String message) {
+        Gdx.app.debug("Server", "received message from " + conn.getRemoteSocketAddress() + ": " + message);
+    }
+
+    @Override
+    public void onMessage(WebSocket conn, ByteBuffer message) {
+        Gdx.app.debug("Server", "received ByteBuffer from " + conn.getRemoteSocketAddress());
+    }
+
+    @Override
+    public void onError(WebSocket conn, Exception ex) {
+        Gdx.app.error("Server", "an error occurred on connection " + conn, ex);
     }
 }
---
+++ server/src/de/golfgl/lightblocks/server/model/Handshake.java
@@ -0,0 +1,8 @@
+package de.golfgl.lightblocks.server.model;
+
+public class Handshake {
+    public int version;
+    public String name;
+    public String owner;
+    public boolean authRequired;
+}
