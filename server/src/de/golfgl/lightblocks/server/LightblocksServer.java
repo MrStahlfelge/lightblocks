35be79b963ada6ff68924f854632384338b3b706
Benjamin Schulte <bs@golfgl.de>
2020-11-02 21:11:28 +0100

    Added a multiplayer server module

---
+++ server/src/de/golfgl/lightblocks/server/LightblocksServer.java
@@ -0,0 +1,91 @@
+package de.golfgl.lightblocks.server;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.backends.headless.HeadlessApplication;
+import com.badlogic.gdx.backends.headless.HeadlessApplicationConfiguration;
+
+import org.java_websocket.WebSocket;
+import org.java_websocket.handshake.ClientHandshake;
+import org.java_websocket.server.WebSocketServer;
+
+import java.net.InetSocketAddress;
+import java.nio.ByteBuffer;
+
+public class LightblocksServer extends WebSocketServer implements ApplicationListener {
+
+    public LightblocksServer(InetSocketAddress address) {
+        super(address);
+    }
+
+    public static void main(String[] arg) {
+        HeadlessApplicationConfiguration config = new HeadlessApplicationConfiguration();
+        int port = 8887;
+
+        LightblocksServer server = new LightblocksServer(new InetSocketAddress(port));
+        new HeadlessApplication(server, config);
+        server.run();
+    }
+
+    @Override
+    public void create() {
+        Gdx.app.log("Server", "Created.");
+    }
+
+    @Override
+    public void resize(int width, int height) {
+
+    }
+
+    @Override
+    public void render() {
+
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void dispose() {
+        Gdx.app.log("Server", "Closed.");
+    }
+
+    @Override
+    public void onOpen(WebSocket conn, ClientHandshake handshake) {
+        conn.send("Welcome to the server!"); //This method sends a message to the new client
+        broadcast("new connection: " + handshake.getResourceDescriptor()); //This method sends a message to all clients connected
+        System.out.println("new connection to " + conn.getRemoteSocketAddress());
+    }
+
+    @Override
+    public void onClose(WebSocket conn, int code, String reason, boolean remote) {
+        System.out.println("closed " + conn.getRemoteSocketAddress() + " with exit code " + code + " additional info: " + reason);
+    }
+
+    @Override
+    public void onMessage(WebSocket conn, String message) {
+        System.out.println("received message from " + conn.getRemoteSocketAddress() + ": " + message);
+    }
+
+    @Override
+    public void onMessage(WebSocket conn, ByteBuffer message) {
+        System.out.println("received ByteBuffer from " + conn.getRemoteSocketAddress());
+    }
+
+    @Override
+    public void onError(WebSocket conn, Exception ex) {
+        System.err.println("an error occurred on connection " + conn.getRemoteSocketAddress() + ":" + ex);
+    }
+
+    @Override
+    public void onStart() {
+        System.out.println("server started successfully");
+    }
+}
