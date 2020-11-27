7ae598ef6b64a332483f4d3ff83bdf53138fc0d5
Benjamin Schulte <bs@golfgl.de>
2020-11-27 22:26:38 +0100

    Server multiplayer: Parse ANT

--- core/src/de/golfgl/lightblocks/multiplayer/ServerMultiplayerModel.java
+++ core/src/de/golfgl/lightblocks/multiplayer/ServerMultiplayerModel.java
@@ -1,10 +1,13 @@
 package de.golfgl.lightblocks.multiplayer;
 
+import com.badlogic.gdx.Gdx;
+
 import de.golfgl.lightblocks.LightBlocksGame;
 import de.golfgl.lightblocks.input.PlayScreenInput;
 import de.golfgl.lightblocks.model.GameModel;
 import de.golfgl.lightblocks.model.GameScore;
 import de.golfgl.lightblocks.model.IGameModelListener;
+import de.golfgl.lightblocks.model.Tetromino;
 import de.golfgl.lightblocks.screen.PlayScreen;
 import de.golfgl.lightblocks.state.InitGameParameters;
 
@@ -88,8 +91,42 @@ public class ServerMultiplayerModel extends GameModel {
         // TODO YNXT-0-0-1-0-1-1-2-1-5
     }
 
-    void handleActivateNextTetro(boolean other, String payload) {
-        // TODO YANT-3-19-4-19-5-19-6-19-0-19
+    void handleActivateNextTetro(final boolean other, final String payload) {
+        final Integer[][] boardBlockPositions = new Integer[Tetromino.TETROMINO_BLOCKCOUNT][2];
+        int pos = parseBlockPositions(payload, 1, boardBlockPositions);
+        final String strBlockType = parseUntilNext(payload, pos, "-");
+        final int blockType = Integer.parseInt(strBlockType);
+        final String strGhostDis = parseUntilNext(payload, pos + strBlockType.length() + 1, "-");
+        final int ghostPieceDistance = Integer.parseInt(strGhostDis);
+
+        Gdx.app.postRunnable(new Runnable() {
+            @Override
+            public void run() {
+                if (!other) {
+                    uiGameboard.activateNextTetro(boardBlockPositions, blockType, ghostPieceDistance);
+                }
+            }
+        });
+    }
+
+    private int parseBlockPositions(String payload, int startpos, Integer[][] boardBlockPositions) {
+        for (int i = 0; i < Tetromino.TETROMINO_BLOCKCOUNT; i++) {
+            for (int p = 0; p < 2; p++) {
+                String nextPos = parseUntilNext(payload, startpos, "-");
+                boardBlockPositions[i][p] = Integer.valueOf(nextPos);
+
+                startpos = startpos + nextPos.length() + 1;
+            }
+        }
+
+        return startpos;
+    }
+
+    private String parseUntilNext(String payload, int startpos, String delimiter) {
+        if (startpos < 0 || startpos >= payload.length())
+            throw new IllegalArgumentException("Can't parse payload from startpos");
+        int nextDelimiter = payload.indexOf(delimiter, startpos);
+        return payload.substring(startpos, nextDelimiter > startpos ? nextDelimiter : payload.length());
     }
 
     void handleSwapHoldAndActive(boolean other, String payload) {
